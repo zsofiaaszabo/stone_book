0bf6b26b8689163c6327527212dfaed8ce67b939
Szabó Zsófia <zsofiaaszabo@gmail.com>
2020-12-17 13:24:37 +0100

    add files

---
+++ main.js
@@ -0,0 +1,21 @@
+function _load(){
+
+/*A feladatokat mindig a lehető legkisebb egységekre bontva, logikailag helytálló formában érdemes leírni, mielőtt bármit csinálnánk. 
+Egy jól megtervezett, felvázolt és részegységekre bontott terv már fél siker a projekt befejezéséhez.*/
+
+/*Kódokat másolni nem érdemes, ha még erre is kerülni sor, részenként a tulajdonságokat, funkciókat megértve és egyenként értelmezve célszerű alkalmazni. 
+Hiszen abban az esetben, ha nem értjük az általunk alkalmazottakat, a végső projektben akadályokba és ellentmondásokba ütközhetünk*/
+
+/*A megfelelő írásjeleket, zárójeleket időben, már a kódok írása során ki kell tenni, nem szabad részleteket lezárások nélkül félbenhagyni. A JavasScript, CSS és HTML
+dokumentumok tagolása során figyelni kell a tagolásra, minél átláthatóbbá kell tenni, mind magunk, mind  harmadikfél számára. Nagy valószínűséggel, ha más nem érti mit írtunk
+mi sem fogjuk érteni, ha később ismét elővesszük a kódunkat*/
+
+
+
+
+
+
+
+
+}
+window.addEventListener("load", _load);
